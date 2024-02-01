9ed6776e9205ed4d7154dfe9dacef7842ee7a680
Reid Roberts <reidroberts24@gmail.com>
2024-01-31 21:10:09 -0600

    added registration validations

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -11,7 +11,9 @@ const Register = ({ history }) => {
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  
+  // State for error messages
+  const [errors, setErrors] = useState({});
+
   const handleRegister = async (e) => {
     e.preventDefault();
     const registrationData = { 
@@ -22,55 +24,93 @@ const Register = ({ history }) => {
       password: password 
     };
     try {
-      console.log(registrationData)
       const response = await axios.post('http://localhost:8000/api/users/register', registrationData, {withCredentials: true});
       navigate('/dashboard')
     } catch (error) {
+      if (error.response && error.response.data && error.response.data.errors) {
+        // Construct an errors object from the array
+        const errorsObj = error.response.data.errors.reduce((acc, curr) => {
+          acc[curr.path] = curr.message;
+          return acc;
+        }, {});
+        setErrors(errorsObj);
+      }
       console.error('Server response:', error.response.data);
     }
   };
+  
 
+  const renderErrorMessage = (field) => {
+    if (errors[field]) {
+      return <span className="error-message">{errors[field]}</span>;
+    }
+    return null;
+  };
+  
+  
   return (
     <div className="register-container">
       <h1>Register</h1>
-      <input
-        type="text"
-        placeholder="First Name"
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-        className="register-input"
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
-        className="register-input"
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="register-input"
-      />
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className="register-input"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="register-input"
-      />
-      <button onClick={handleRegister} className="register-button">
-        Register
-      </button>
+      <form onSubmit={handleRegister} className="register-form">
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            className="register-input"
+          />
+          {renderErrorMessage('first_name')}
+        </div>
+  
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            className="register-input"
+          />
+          {renderErrorMessage('last_name')}
+        </div>
+  
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="register-input"
+          />
+          {renderErrorMessage('email')}
+        </div>
+  
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="register-input"
+          />
+          {renderErrorMessage('username')}
+        </div>
+  
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="register-input"
+          />
+          {renderErrorMessage('password')}
+        </div>
+  
+        <button type="submit" className="register-button">
+          Register
+        </button>
+      </form>
     </div>
   );
 };
