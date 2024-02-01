c5b236d2030ef171e91ed415dc1f4cfe445203fb
Reid Roberts <reidroberts24@gmail.com>
2024-01-31 23:01:56 -0600

    updates

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -8,19 +8,19 @@ const Posts = ({ userId }) => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/api/posts');
-        setPosts(response.data.posts.map(post => ({
-          ...post,
-          likeCount: Number.isInteger(post.likeCount) ? post.likeCount : 0
-        })));
-      } catch (error) {
-        console.error('Error fetching posts:', error);
-      }
+        try {
+            const response = await axios.get('http://localhost:8000/api/posts');
+            setPosts(response.data.posts.map(post => ({
+                ...post,
+                likeCount: Number.isInteger(post.likeCount) ? post.likeCount : 0
+            })));
+        } catch (error) {
+            console.error('Error fetching posts:', error);
+        }
     };
 
     fetchPosts();
-  }, [userId]);
+}, [userId]);
 
   const handleLike = async (postId) => {
     try {
@@ -52,32 +52,41 @@ const Posts = ({ userId }) => {
     }));
   };
 
+  const deletePost = async (postId) => {
+    try {
+        await axios.delete(`http://localhost:8000/api/posts/${postId}`);
+        setPosts(posts.filter(post => post.id !== postId));
+    } catch (error) {
+        console.error('Error deleting post:', error);
+    }
+  };
+
   return (
     <div className="dashboard-posts-container">
-      <h2>Posts</h2>
-      {posts.length === 0 ? (
-        <p>No posts available.</p>
-      ) : (
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>
-              <strong>Caption:</strong> {post.caption}
-              {/* Render media content if available */}
-              {post.media_content && (
-                <img src={post.media_content} alt="Post Media" />
-              )}
-              <div>
-                <span>Likes: {post.likeCount}</span>
-                {likedPosts.has(post.id) ? (
-                  <button onClick={() => handleUnlike(post.id)}>Unlike</button>
-                ) : (
-                  <button onClick={() => handleLike(post.id)}>Like</button>
-                )}
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+        <h2>Posts</h2>
+        {posts.length === 0 ? (
+            <p>No posts available.</p>
+        ) : (
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <strong>Caption:</strong> {post.caption}
+                        {post.media_content && (
+                            <img src={post.media_content} alt="Post Media" />
+                        )}
+                        <div>
+                            <span>Likes: {post.likeCount}</span>
+                            {likedPosts.has(post.id) ? (
+                                <button onClick={() => handleUnlike(post.id)}>Unlike</button>
+                            ) : (
+                                <button onClick={() => handleLike(post.id)}>Like</button>
+                            )}
+                            <button onClick={() => deletePost(post.id)}>Delete</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        )}
     </div>
   );
 };
--- server/routes/friendRequest.routes.js
+++ server/routes/friendRequest.routes.js
@@ -2,10 +2,10 @@ const FriendRequestController = require('../controllers/friendRequest.controller
 const { authenticate } = require('../config/jwt.config');
 
 module.exports = (app) => {
-    app.post('/api/friendRequests/send', authenticate, FriendRequestController.sendFriendRequest); // --> send new friend request to user
-    app.post('/api/friendRequests/accept', authenticate, FriendRequestController.acceptFriendRequest); // --> accept friend request from other user
-    app.post('/api/friendRequests/decline', authenticate, FriendRequestController.declineFriendRequest); // --> decline friend request from other user
-    app.get('/api/friendRequests/list/:userId', authenticate, FriendRequestController.listFriendRequests); // --> list user's friend requests
-    app.get('/api/friends/list/:userId', authenticate, FriendRequestController.listFriends); // --> list user's friends (that were accepted)
-    app.post('/api/friends/remove', authenticate, FriendRequestController.removeFriendship); // --> remove a friend ffrom user's friend list
+    app.post('/api/friendRequests/send', FriendRequestController.sendFriendRequest); // --> send new friend request to user
+    app.post('/api/friendRequests/accept', FriendRequestController.acceptFriendRequest); // --> accept friend request from other user
+    app.post('/api/friendRequests/decline', FriendRequestController.declineFriendRequest); // --> decline friend request from other user
+    app.get('/api/friendRequests/list/:userId', FriendRequestController.listFriendRequests); // --> list user's friend requests
+    app.get('/api/friends/list/:userId',  FriendRequestController.listFriends); // --> list user's friends (that were accepted)
+    app.post('/api/friends/remove',  FriendRequestController.removeFriendship); // --> remove a friend ffrom user's friend list
 }
