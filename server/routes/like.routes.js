6d0e1f6158566051a304bfd50e3aa35e031627f3
Reid Roberts <reidroberts24@gmail.com>
2024-01-31 21:55:05 -0600

    upadted dashboard and Posts

--- server/routes/like.routes.js
+++ server/routes/like.routes.js
@@ -2,7 +2,7 @@ const LikeController = require('../controllers/like.controller');
 const { authenticate } = require('../config/jwt.config');
 
 module.exports = (app) => {
-    app.post('/api/likes/add', authenticate, LikeController.addLike); // --> add new like to a user's post
-    app.get('/api/likes/:postId', authenticate, LikeController.getLikesByPost); // --> get all the likes on a user's post
-    app.delete('/api/likes', authenticate, LikeController.removeLike); // --> remove user's like from a post
+    app.post('/api/likes/add', LikeController.addLike); // --> add new like to a user's post
+    app.get('/api/likes/:postId', LikeController.getLikesByPost); // --> get all the likes on a user's post
+    app.delete('/api/likes', LikeController.removeLike); // --> remove user's like from a post
 }
